4542e143b0b24a565490471dc343bc1fa717d556
unknown <alexis.platino@gmail.com>
2020-05-12 19:25:48 +0200

    Workaround to reduce cpu weight on getting the mac

--- src/macParser.js
+++ src/macParser.js
@@ -1,9 +1,5 @@
 const xmlParser = require("xml-js");
 const path = require("path");
-const xml = require("fs").readFileSync(
-    path.join(__dirname, "macaddress.io-db.xml")
-);
-const parsed = xmlParser.xml2json(xml, { compact: true, spaces: 4 });
 
 class Parser {
     constructor() {
@@ -11,28 +7,41 @@ class Parser {
         this.init();
     }
     init() {
+        const xml = require("fs").readFileSync(
+            path.join(__dirname, "macaddress.io-db.xml")
+        );
+        const parsed = xmlParser.xml2json(xml, { compact: true, spaces: 4 });
         const macs = JSON.parse(parsed);
         // IDFK who made this xml but it has to be like this
         this.macs = macs.records.record;
     }
 
     parseData(data, mac) {
-        if (!data) return {}
+        if (!data) return {};
         // It has to be done like this because it has this shit attribute _text
-        const mapped = Object.keys(data).reduce((object, key) => {
-            object[key] = data[key]._text;
-            return object;
-        }, {});
-        return mapped;
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const mapped = Object.keys(data).reduce((object, key) => {
+                    object[key] = data[key]._text;
+                    return object;
+                }, {});
+                resolve(mapped);
+            }, 1);
+        });
     }
 
     getData(mac) {
         if (mac === true) return {};
-        const firstThree = mac.match("^(.{8})")[0].toUpperCase();
-        const dataFound = this.macs.find(
-            ({ oui: { _text } }) => _text === firstThree
-        );
-        return this.parseData(dataFound, mac);
+        return new Promise((resolve) => {
+            setTimeout(async () => {
+                const firstThree = mac.match("^(.{8})")[0].toUpperCase();
+                const dataFound = this.macs.find(
+                    ({ oui: { _text } }) => _text === firstThree
+                );
+                const parsedData = await this.parseData(dataFound, mac);
+                resolve(parsedData);
+            }, 1);
+        });
     }
 }
 
